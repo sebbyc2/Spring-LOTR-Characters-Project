37a718451798ee56da475ee1d881dba38d3d8cc8
sebby <sebbyc2@gmail.com>
2022-02-07 11:54:42 +0000

    Compelted controller V1

---
+++ src/main/java/com/qa/lotrcharactersproject/rest/CharacterController.java
@@ -0,0 +1,52 @@
+package com.qa.lotrcharactersproject.rest;
+
+import com.qa.lotrcharactersproject.domain.LOTRCharacter;
+import com.qa.lotrcharactersproject.service.CharacterService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/LOTRCharacter")
+public class CharacterController {
+
+    //Initialise a service
+    private CharacterService service;
+
+    //Construct a controller that uses the service
+    public CharacterController(CharacterService service) {
+        this.service = service;
+    }
+
+    //POST - Create Character
+    @PostMapping
+    public ResponseEntity<LOTRCharacter> create(LOTRCharacter character) {
+        return new ResponseEntity<>(this.service.create(character), HttpStatus.CREATED);
+    }
+
+    //GET - Read All
+    @GetMapping
+    public ResponseEntity<List<LOTRCharacter>> readAll() {
+        return new ResponseEntity<>(this.service.readAll(), HttpStatus.FOUND);
+    }
+
+    //GET - Read by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<LOTRCharacter> readById(Long id) {
+        return new ResponseEntity<>(this.service.readById(id), HttpStatus.FOUND);
+    }
+
+    //PUT - Update Character
+    @PutMapping("/{id}")
+    public ResponseEntity<LOTRCharacter> update(LOTRCharacter character, Long id) {
+        return new ResponseEntity<>(this.service.update(character, id), HttpStatus.ACCEPTED);
+    }
+
+    //DELETE - Delete Character by ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> delete(Long id) {
+        return new ResponseEntity<>(this.service.delete(id), HttpStatus.OK);
+    }
+}
