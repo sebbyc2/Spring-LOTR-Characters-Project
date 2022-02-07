115c93b7a2c9e68260d85cde948e2c2e69a8269e
sebby <sebbyc2@gmail.com>
2022-02-07 13:12:27 +0000

    added constructorTestWithId Test

--- src/test/java/com/qa/lotrcharactersproject/domain/LOTRCharacterTest.java
+++ src/test/java/com/qa/lotrcharactersproject/domain/LOTRCharacterTest.java
@@ -3,12 +3,23 @@ package com.qa.lotrcharactersproject.domain;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 public class LOTRCharacterTest {
 
     @Test
     public void constructorTestWithId(){
-        LOTRCharacter character = new LOTRCharacter(1L, "Gandalf", 24000, "Maia");
+        LOTRCharacter character = new LOTRCharacter(2L, "Aragorn", 87, "Human");
+        assertNotNull(character.getId());
+        assertNotNull(character.getName());
+        assertNotNull(character.getAge());
+        assertNotNull(character.getRace());
+        assertEquals(2L, character.getId());
+        assertEquals("Aragorn", character.getName());
+        assertEquals(87, character.getAge());
+        assertEquals("Human", character.getRace());
 
     }
 }
